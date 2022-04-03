ee702f0878ea1c1b4d5409ea2f1e3ee7219a3059
HellOwhatAs <88815487+HellOwhatAs@users.noreply.github.com>
2022-04-03 23:01:22 +0800

    Update mybdfy.py

--- mybdfy.py
+++ mybdfy.py
@@ -58,13 +58,21 @@ else:
     with open("mybdfy_hea.mybdfy","w") as f:
         pass
     raise Exception("please fill the \"mybdfy_hea.mybdfy\" with your headers")
-data={'from': 'en', 'to': 'zh', 'query': 'none', 'transtype': 'realtime', 'simple_means_flag': '3', 'sign': 'none', 'token': '692573090713f1ab5d06a7392dfc3f57', 'domain': 'common'}
+if os.path.exists("mybdfy_token.mybdfy"):
+    with open("mybdfy_token.mybdfy","r") as f:
+        token=f.read()
+else:
+    with open("mybdfy_token.mybdfy","w") as f:
+        pass
+    raise Exception("please fill the \"mybdfy_token.mybdfy\" with your token in the form")
+data={'from': 'en', 'to': 'zh', 'query': 'none', 'transtype': 'realtime', 'simple_means_flag': '3', 'sign': 'none', 'token': 'none', 'domain': 'common'}
 langlist={'zh': '中文','jp': '日语','jpka': '日语假名','th': '泰语','fra': '法语','en': '英语','spa': '西班牙语','kor': '韩语','tr': '土耳其语','vie': '越南语','ms': '马来语','de': '德语','ru': '俄语','ir': '伊朗语','ara': '阿拉伯语','est': '爱沙尼亚语','be': '白俄罗斯语','bul': '保加利亚语','hi': '印地语','is': '冰岛语','pl': '波兰语','fa': '波斯语','dan': '丹麦语','tl': '菲律宾语','fin': '芬兰语','nl': '荷兰语','ca': '加泰罗尼亚语','cs': '捷克语','hr': '克罗地亚语','lv': '拉脱维亚语','lt': '立陶宛语','rom': '罗马尼亚语','af': '南非语','no': '挪威语','pt_BR': '巴西语','pt': '葡萄牙语','swe': '瑞典语','sr': '塞尔维亚语','eo': '世界语','sk': '斯洛伐克语','slo': '斯洛文尼亚语','sw': '斯瓦希里语','uk': '乌克兰语','iw': '希伯来语','el': '希腊语','hu': '匈牙利语','hy': '亚美尼亚语','it': '意大利语','id': '印尼语','sq': '阿尔巴尼亚语','am': '阿姆哈拉语','as': '阿萨姆语','az': '阿塞拜疆语','eu': '巴斯克语','bn': '孟加拉语','bs': '波斯尼亚语','gl': '加利西亚语','ka': '格鲁吉亚语','gu': '古吉拉特语','ha': '豪萨语','ig': '伊博语','iu': '因纽特语','ga': '爱尔兰语','zu': '祖鲁语','kn': '卡纳达语','kk': '哈萨克语','ky': '吉尔吉斯语','lb': '卢森堡语','mk': '马其顿语','mt': '马耳他语','mi': '毛利语','mr': '马拉提语','ne': '尼泊尔语','or': '奥利亚语','pa': '旁遮普语','qu': '凯楚亚语','tn': '塞茨瓦纳语','si': '僧加罗语','ta': '泰米尔语','tt': '塔塔尔语','te': '泰卢固语','ur': '乌尔都语','uz': '乌兹别克语','cy': '威尔士语','yo': '约鲁巴语','yue': '粤语','wyw': '文言文','cht': '中文繁体'}
 def translate(x:str,_from="en",_to="zh",_max_try=None):
     data["from"]=_from
     data["to"]=_to
     data["query"]=x
     data["sign"]=sign_js.call("e",x)
+    data["token"]=token
     if _max_try==None:
         while True:
             resp=requests.post("https://fanyi.baidu.com/v2transapi?from={}&to={}".format(_from,_to),headers={i[:i.find(":")].strip():i[i.find(":")+1:].strip() for i in hea.split("\n") if i.strip()},data=data)
@@ -86,3 +94,5 @@ def splitlist(x:list,n=10):
     ret=[x[tm[i]:tm[i+1]] for i in range(len(tm)-1)]
     ret.append(x[tm[-1]:])
     return ret
+if __name__=="__main__":
+    print(translate("hello world"))
